a4c99b1e5177bd8b85f01f74b0eca6783edfba1c
Jun <juninfo1997@gmail.com>
2018-04-24 19:46:47 +0200

    login not working

--- Bisai_Android-master/app/src/main/java/com/bisai/bisai/controller/managers/TeamMenu.java
+++
@@ -1,9 +0,0 @@
-package com.bisai.bisai.controller.managers;
-
-/**
- * Created by DAM on 3/5/17.
- */
-
-public class TeamMenu {
-
-}
--- Bisai_Android-master/app/src/main/java/com/bisai/bisai/util/CustomProperties.java
+++
@@ -1,9 +0,0 @@
-package com.bisai.bisai.util;
-
-public class CustomProperties {
-    public static String clientId = "bisaiapp";
-    public static String clientSecret = "my-secret-token-to-change-in-production";
-    public static String grantType = "password";
-    public static String scope = "read write";
-    public static String baseUrl = "http://192.168.22.62:8080";
-}
