04e41bd43d9692114bfaa8db601f4464c0adab05
Jun <juninfo1997@gmail.com>
2018-05-07 20:19:16 +0200

    Show Articles not working

--- app/src/main/java/com/example/alu2015059/jstyle/PaginaArticulos.java
+++ app/src/main/java/com/example/alu2015059/jstyle/PaginaArticulos.java
@@ -1,21 +1,16 @@
 package com.example.alu2015059.jstyle;
 
-import android.app.ListActivity;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.alu2015059.jstyle.SQLite.SQLiteDBHelper;
 
 /**
  * Created by alu2015059 on 23/01/2018.
@@ -24,11 +19,12 @@ import java.util.List;
 public class PaginaArticulos extends AppCompatActivity{
     private ListView listView;
     private Button btn_buscar;
+    private EditText code;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pagina_articulos);
+        setContentView(R.layout.pagina_articulos_adm);
 
         //Boton para volver a la pagina principal
         Button btn_volver = findViewById(R.id.btn_volver);
@@ -43,7 +39,24 @@ public class PaginaArticulos extends AppCompatActivity{
         btn_buscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                code = findViewById(R.id.et_buscador);
+                String codigo = String.valueOf(code);
 
+                if(!codigo.isEmpty()){
+                    SQLiteDBHelper SQLiteDBHelper = new SQLiteDBHelper(PaginaArticulos.this);
+                    SQLiteDatabase sqLiteDatabase = SQLiteDBHelper.getWritableDatabase();
+
+                    String query = "SELECT * FORM ARTICULOS WHERE codigo = '" + codigo + "'";
+
+                    if(query = ""){
+
+                    }
+
+                    SQLiteDBHelper.getArticuloByCodigo(codigo);
+                    sqLiteDatabase.close();
+                }else{
+                    Toast.makeText(PaginaArticulos.this, "Buscador vacio",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
